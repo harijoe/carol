668e4dae6d77b99ce8fc8744de9f2ac327106401
yanli <delphine.li0427@gmail.com>
2016-10-19 14:46:19 +0200

    hot fix (#42)
    fix page home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <img
-        alt=""
-        src="https://ssl.gstatic.com/onebox/media/olympics/photos/o16/live/RIOEC8C1QFE71_768x432.JPG"
-      />
-  </div>
+      <img alt="" src="https://ssl.gstatic.com/onebox/media/olympics/photos/o16/live/RIOEC8C1QFE71_768x432.JPG" />
+    </div>
   )
 }
 
