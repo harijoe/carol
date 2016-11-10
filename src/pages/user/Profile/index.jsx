939287a4045847786db50802b1790240c83f69ef
maily1987 <mailypaquet@gmail.com>
2016-11-10 14:23:37 +0100

    Modify edit profile

--- src/pages/user/Profile/index.jsx
+++ src/pages/user/Profile/index.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { getProfile, updateProfile } from './ducks'
+import InputText from '../../../ui/form/input/Text'
+import InputRadio from '../../../ui/form/input/Radio'
 import InputPhone from '../../../ui/form/input/Phone'
+import InputPostal from '../../../ui/form/input/Postal'
 import Form from '../../../ui/form/Form'
 import Button from '../../../ui/form/input/Button'
 
@@ -14,7 +17,14 @@ class Profile extends Component {
     this.onSubmit = this.onSubmit.bind(this)
     this.checkAccess = this.checkAccess.bind(this)
     this.state = {
-      phone: ''
+      gender1: 'Mr',
+      gender2: 'Mrs',
+      firstName: '',
+      lastName:'',
+      mobilePhone: '',
+      fixedPhone: '',
+      address: '',
+      zipCode: ''
     }
   }
 
@@ -36,13 +46,25 @@ class Profile extends Component {
 
   onSubmit() {
     this.props.updateProfile({
-      phone: this.state.phone,
+      gender: this.state.gender,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      mobilePhone: this.state.mobilePhone,
+      fixedPhone: this.state.fixedPhone,
+      address: this.state.address,
+      zipCode: this.state.zipCode,
     }, this.props.user.get('id'))
   }
 
   setFields(data) {
     this.setState({
-      phone: data.get('phone')
+      gender: data.get('gender'),
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      mobilePhone: data.get('mobilePhone'),
+      fixedPhone: data.get('fixedPhone'),
+      address: data.get('address'),
+      zipCode: data.get('zipCode')
     })
   }
 
@@ -57,28 +79,186 @@ class Profile extends Component {
   }
 
   handleChange(e) {
-    this.setState({
-      phone: e.target.value
-    })
+    switch (e.target.name) {
+      case 'gender':
+        this.setState({
+          gender: e.target.value
+        })
+
+        break
+      case 'firstName':
+        this.setState({
+          firstName: e.target.value
+        })
+
+        break
+      case 'lastName':
+        this.setState({
+          lastName: e.target.value
+        })
+
+        break
+      case 'mobilePhone':
+        this.setState({
+          mobilePhone: e.target.value
+        })
+
+        break
+      case 'fixedPhone':
+        this.setState({
+          fixedPhone: e.target.value
+        })
+
+        break
+      case 'address':
+        this.setState({
+          address: e.target.value
+        })
+
+        break
+      case 'zipCode':
+        this.setState({
+          zipCode: e.target.value
+        })
+
+        break
+      default:
+        return
+    }
   }
 
   render() {
-    const attrPhone = {
-      className: 'phone',
-      placeholder: 'Phone',
-      id: 'phone',
-      name: 'phone'
+    const attrGender1 = {
+      className: 'gender',
+      id: 'gender1',
+      placeholder: 'Gender',
+      name: 'gender',
+      required: 'required'
+    }
+
+    const attrGender2 = {
+      className: 'gender',
+      id: 'gender2',
+      placeholder: 'Gender',
+      name: 'gender',
+      required: 'required'
+    }
+
+    const attrFirstName = {
+      className: 'firstName',
+      id: 'firstName',
+      placeholder: 'First name',
+      name: 'firstName',
+      required: 'required'
+    }
+
+    const attrLastName = {
+      className: 'lastName',
+      id: 'lastName',
+      placeholder: 'Last name',
+      name: 'lastName',
+      required: 'required'
+    }
+
+    const attrMobilePhone = {
+      className: 'mobilePhone',
+      id: 'mobilePhone',
+      placeholder: 'Mobile phone number',
+      name: 'mobilePhone',
+      required: 'required'
+    }
+
+    const attrFixedPhone = {
+      className: 'fixedPhone',
+      id: 'fixedPhone',
+      placeholder: 'Fixed phone number',
+      name: 'fixedPhone',
+      required: 'required'
+    }
+
+    const attrAddress = {
+      className: 'address',
+      id: 'address',
+      placeholder: 'Address',
+      name: 'address',
+      required: 'required'
+    }
+
+    const attrZipCode = {
+      className: 'zipCode',
+      id: 'zipCode',
+      placeholder: 'Postal code',
+      name: 'zipCode',
+      required: 'required'
     }
 
     return (
       <div>
         <Form onSubmit={this.onSubmit}>
-          <InputPhone
-            attr={attrPhone}
-            value={this.state.phone}
-            onChange={this.handleChange}
-          />
-          <Button type="submit" value="Profile" />
+          <div className="error">
+            { this.props.auth.get('error') } <br />
+            { this.props.profile.get('error') } <br />
+            { this.state.error }
+          </div>
+          <div className="form-group">
+            <InputRadio
+              attr={attrGender1}
+              value={this.state.gender1}
+              onChange={this.handleChange}
+            />
+            Mr
+            <InputRadio
+              attr={attrGender2}
+              value={this.state.gender2}
+              onChange={this.handleChange}
+            />
+            Mrs
+          </div>
+          <div className="form-group">
+            <InputText
+              attr={attrFirstName}
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <InputText
+              attr={attrLastName}
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <InputPhone
+              attr={attrMobilePhone}
+              value={this.state.mobilePhone}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <InputPhone
+              attr={attrFixedPhone}
+              value={this.state.fixedPhone}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <InputText
+              attr={attrAddress}
+              value={this.state.address}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <InputPostal
+              attr={attrZipCode}
+              value={this.state.zipCode}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <Button type="submit" value="Profile" />
+          </div>
         </Form>
       </div>
     )
