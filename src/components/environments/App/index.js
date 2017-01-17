eaeb77d8d4a9ea03af3fe209b0c58ce29c741dab
Gueorgui Agapov <7s4r@users.noreply.github.com>
2017-01-17 13:57:07 +0100

    Add react helmet (#113)

--- src/components/environments/App/index.js
+++ src/components/environments/App/index.js
@@ -1,10 +1,29 @@
 import React, { PropTypes } from 'react'
 import { injectGlobal } from 'styled-components'
+import Helmet from 'react-helmet'
 
 injectGlobal(['body { margin: 0; }'])
 
 const App = props => (
-  <div>{props.children}</div>
+  <div>
+    <Helmet
+      title="Comparer les prix avec 5 devis gratuits"
+      titleTemplate="Devis Travaux - %s"
+      meta={[
+        { name: 'description', content: 'Quotatis vous permet d&apos;obtenir 5 devis gratuits sous trois jours. Demandes de devis comparatifs au 01.77.86.80.97 : devis travaux, devis pompe a chaleur, devis chauffage, devis defiscalisation, devis fenêtres, devis assurances, devis piscine, devis fenetres, devis climatisation, devis panneaux solaire photovoltaiques...' },
+        { name:'viewport', content:'width=device-width, initial-scale=1' },
+        { property: 'og:site_name', content: 'Quotatis' },
+        { property: 'og:title', content: 'Devis Travaux - Comparer les prix avec 5 devis gratuits' },
+        { property: 'og:image', content: 'https://lh5.googleusercontent.com/-H8MZCl-4zME/UI-VaVpuRjI/AAAAAAAAAAc/rc9uPeRIwsc/w851-h315-no/Quotatis-fond-googleplus.jpg' },
+        { property: 'og:image:type', content: 'image/jpg' },
+        { property: 'og:url', content: 'https://quotatis.fr' },
+      ]}
+      link={[
+        { rel: 'icon', href: 'https://quotatis.fr/favicon.ico' }
+      ]}
+    />
+    {props.children}
+  </div>
 )
 
 App.propTypes = {
