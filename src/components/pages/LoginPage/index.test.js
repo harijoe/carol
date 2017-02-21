c655d4602cd6a58f7d82d1e9f4eb9d04d3b3a6bc
Julien Vallini <vallinij@gmail.com>
2017-02-21 17:07:13 +0100

    Add login page snapshots

--- src/components/pages/LoginPage/index.test.js
+++ src/components/pages/LoginPage/index.test.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import LoginPage from './'
 
-it('renders', () => {
-  shallow(<LoginPage />)
+it('renders LoginPage', () => {
+  expect(shallow(<LoginPage />)).toMatchSnapshot()
 })
