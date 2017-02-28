2bf71dc06643cb46efd1b3175805760d52e234ff
Stephen Samar <dev@thewellcom.fr>
2017-02-28 18:23:50 +0100

    hotfix: Add social connection in sign up form (#186)

--- src/components/organisms/SignUpForm/index.js
+++ src/components/organisms/SignUpForm/index.js
@@ -3,7 +3,7 @@ import { Field } from 'redux-form'
 import styled from 'styled-components'
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
 
-import { RenderField, RenderCaptcha, Heading, Button } from 'components'
+import { RenderField, RenderCaptcha, Heading, Button, FacebookLogin, GoogleLogin } from 'components'
 import messages from 'utils/messages'
 
 const Form = styled.form`
@@ -17,16 +17,20 @@ const StyledButton = styled(Button)`
 `
 
 const SignUpForm = ({ handleSubmit, submitting, intl }) => (
-  <Form onSubmit={handleSubmit}>
-    <Heading level={2}><FormattedMessage id="user.sign_up.heading" /></Heading>
-    <Field name="_csrf" type="hidden" component="input" />
-    <Field name="email" label={intl.formatMessage(messages('user.email').label)} component={RenderField} />
-    <Field name="password" type="password" label={intl.formatMessage(messages('user.password').label)} component={RenderField} />
-    <Field name="confirmPassword" type="password" label={intl.formatMessage(messages('user.confirm_password').label)} component={RenderField} />
-    <Field name="newsletterSubscription" type="checkbox" label={intl.formatMessage(messages('user.newsletter_subscription').label)} component={RenderField} />
-    <Field name="captcha" component={RenderCaptcha} />
-    <StyledButton type="submit" disabled={submitting}><FormattedMessage id="user.sign_up" /></StyledButton>
-  </Form>
+  <div>
+    <Form onSubmit={handleSubmit}>
+      <Heading level={2}><FormattedMessage id="user.sign_up.heading" /></Heading>
+      <Field name="_csrf" type="hidden" component="input" />
+      <Field name="email" label={intl.formatMessage(messages('user.email').label)} component={RenderField} />
+      <Field name="password" type="password" label={intl.formatMessage(messages('user.password').label)} component={RenderField} />
+      <Field name="confirmPassword" type="password" label={intl.formatMessage(messages('user.confirm_password').label)} component={RenderField} />
+      <Field name="newsletterSubscription" type="checkbox" label={intl.formatMessage(messages('user.newsletter_subscription').label)} component={RenderField} />
+      <Field name="captcha" component={RenderCaptcha} />
+      <StyledButton type="submit" disabled={submitting}><FormattedMessage id="user.sign_up" /></StyledButton>
+    </Form>
+    <FacebookLogin />
+    <GoogleLogin />
+  </div>
 )
 
 SignUpForm.propTypes = {
