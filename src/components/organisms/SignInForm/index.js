3863504131eb43f15cd5c45c8c699869ced06ef4
Amrouche Hamza <laharl.moon@gmail.com>
2017-03-02 18:04:32 +0100

    hotfix: use translation for email / password
    * hotfix: use translation for email / password
    
    * format message

--- src/components/organisms/SignInForm/index.js
+++ src/components/organisms/SignInForm/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Field } from 'redux-form'
 import styled from 'styled-components'
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
 
 import { RenderField, Button, Link, FacebookLogin, GoogleLogin } from 'components'
+import messages from 'utils/messages'
 
 const Form = styled.form`
   width: 100%;
@@ -17,12 +18,12 @@ const StyledLink = styled(Link)`
   margin: 10px 0;
 `
 
-const SignInForm = ({ error, handleSubmit, submitting }) => (
+const SignInForm = ({ error, handleSubmit, submitting, intl: { formatMessage } }) => (
   <div>
     <Form onSubmit={handleSubmit}>
       {error && <FormattedMessage id={error} tagName="strong" />}
-      <Field name="email" type="text" component={RenderField} label="Email" />
-      <Field name="password" type="password" component={RenderField} label="Password" />
+      <Field name="email" type="text" component={RenderField} label={formatMessage(messages('user.email').label)} />
+      <Field name="password" type="password" component={RenderField} label={formatMessage(messages('user.password').label)} />
       <Button type="submit" disabled={submitting}><FormattedMessage id="user.sign_in" tagName="span" /></Button>
     </Form>
     <StyledLink to="/signup"><FormattedMessage id="user.create_account" /></StyledLink>
@@ -36,6 +37,7 @@ SignInForm.propTypes = {
   handleSubmit: React.PropTypes.func,
   submitting: React.PropTypes.bool,
   error: React.PropTypes.string,
+  intl: intlShape.isRequired,
 }
 
-export default SignInForm
+export default injectIntl(SignInForm)
--- src/components/organisms/SignInForm/index.test.js
+++ src/components/organisms/SignInForm/index.test.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import SignInForm from '.'
+import intlMock from '../../../../test/intlMock'
 
-const wrap = (props = {}) => shallow(<SignInForm {...props} />)
+const wrap = (props = {}) => shallow(intlMock(<SignInForm {...props} />))
 
 it('renders SignInForm', () => {
   expect(wrap()).toMatchSnapshot()
