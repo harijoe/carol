a38475123b725068f493c054a66c14d44b324e4b
Julien Deltruel <deltruelj@gmail.com>
2017-04-07 15:37:20 +0200

    hotfix: visible dots (#276)

--- src/components/atoms/Carousel/index.js
+++ src/components/atoms/Carousel/index.js
@@ -8,15 +8,22 @@ import styles from './styles'
 const Wrapper = styled.div`${styles}`
 
 const StyledSlickCarousel = styled(SlickCarousel)`
-  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: visible;
+  padding-bottom: ${theme('spaces.m')};
 
   ${mapBreakpoints(bp => css`
     margin-left: calc(${theme(`grid.gutterWidth.${bp}`, 'rem')} * -1);
     margin-right: calc(${theme(`grid.gutterWidth.${bp}`, 'rem')} * -1);
 
     > .slick-list {
+      overflow-y: visible;
       padding-left: ${theme(`grid.gutterWidth.${bp}`, 'rem')};
       padding-right: ${theme(`grid.gutterWidth.${bp}`, 'rem')};
+    }
+
+    > .slick-dots {
+      bottom: 0;
     }
  `)}
 `
