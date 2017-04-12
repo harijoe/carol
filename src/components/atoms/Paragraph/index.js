d591df3d90241b352920fc48019423ac4be0fa0a
Julien Deltruel <deltruelj@gmail.com>
2017-04-12 16:18:01 +0200

    Fix missing font for paragraph (#289)

--- src/components/atoms/Paragraph/index.js
+++ src/components/atoms/Paragraph/index.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 import { theme } from 'utils/style'
 
 const Paragraph = styled.p`
+  font: normal ${theme('fonts.size.base')} montserrat-light, sans-serif !important;
   color: inherit;
-  font-size: ${theme('fonts.size.base')};
   line-height: 1.3;
   margin: ${theme('spaces.m')} 0 0 0;
 `
