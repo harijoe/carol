aa4eb7780707cd548cf86bd0e72e0db477f1f2ef
sylvain-dion <sylvain.dion@gmail.com>
2017-04-19 21:30:47 +0200

    Links and Buttons variables (#304)

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,14 +1,41 @@
 import React, { PropTypes } from 'react'
-import { theme, ifThen } from 'utils/style'
+import { theme, ifThen, breakpoint } from 'utils/style'
 import styled, { css } from 'styled-components'
 
-const StyledButton = styled.button`${({ block }) => css`
+const StyledButton = styled.button`${({ block, secondary, maxWidth, center }) => css`
   display: block;
-  ${ifThen(block, 'width: 100%')};
-  border: 0;
-  outline: 0;
+  margin: ${theme('spaces.m')} 0 0 0;
+  padding: ${theme('spaces.m')};
+  min-width: 20rem;
+  font-family: ${theme('fonts.family.montserratBold')};
+  font-size: ${theme('fonts.size.base')};
+  letter-spacing: .05rem;
+  text-align: center;
   background-color: ${theme('colors.secondary')};
-  padding: 15px;
+  border: 0;
+  color: ${theme('colors.black')};
+  outline: 0;
+
+  ${breakpoint('m')`
+    padding: ${theme('spaces.l')};
+    margin-bottom: ${theme('spaces.l')};
+    margin-top: ${theme('spaces.l')};
+  `}
+
+  ${ifThen(block, 'width: 100%')};
+  ${ifThen(secondary, css`
+    background-color: ${theme('colors.grayscale.lighter')};
+    color: ${theme('colors.grayscale.dark')};
+  `)};
+  ${ifThen(maxWidth, css`
+    ${breakpoint('m')`
+      max-width: 32rem;
+    `}
+  `)};
+  ${ifThen(center, css`
+    margin-left: auto;
+    margin-right: auto;
+  `)};
 `}`
 
 const Button = ({ type, ...props }) => <StyledButton {...props} type={type} />
@@ -16,6 +43,9 @@ const Button = ({ type, ...props }) => <StyledButton {...props} type={type} />
 Button.propTypes = {
   disabled: PropTypes.bool,
   block: PropTypes.bool,
+  secondary: PropTypes.bool,
+  maxWidth: PropTypes.bool,
+  center: PropTypes.bool,
   type: PropTypes.string,
 }
 
--- src/components/atoms/Link/index.js
+++ src/components/atoms/Link/index.js
@@ -1,15 +1,68 @@
 import React, { PropTypes } from 'react'
 import styled, { css } from 'styled-components'
 import { Link as RouterLink } from 'react-router'
-import { theme } from 'utils/style'
+import { theme, ifThen, breakpoint } from 'utils/style'
 
-const styles = ({ kind }) => css`
+const styles = ({ kind, highlight, button }) => css`
   text-decoration: none;
   color: ${theme(`colors.${kind}`)};
 
   &:hover {
     text-decoration: none;
   }
+
+  ${ifThen(highlight, css`
+    position: relative;
+    z-index: 0;
+    padding-left: ${theme('spaces.xs')};
+    padding-right: ${theme('spaces.xs')};
+    color: inherit;
+    transition: color 0.3s 0.1s ease;
+
+    &::before {
+      position: absolute;
+      bottom: -${theme('spaces.xs')};
+      left: 0;
+      z-index: -1;
+      height: calc(50% + ${theme('spaces.xs')});
+      width: 100%;
+      background-color: ${theme('colors.primary')};
+      content: '';
+      opacity: 0.2;
+      transition: all 0.3s ease;
+    }
+
+    &:hover {
+      color: ${theme('colors.white')};
+
+      &::before {
+        height: calc(100% + ${theme('spaces.s')});
+        opacity: 1;
+      }
+    }
+  `)};
+
+  ${ifThen(button, css`
+    display: block;
+    margin: ${theme('spaces.m')} 0 0 0;
+    padding: ${theme('spaces.m')};
+    min-width: 20rem;
+    font-family: ${theme('fonts.family.montserratBold')};
+    font-size: ${theme('fonts.size.base')};
+    letter-spacing: .05rem;
+    text-align: center;
+    background-color: ${theme('colors.secondary')};
+    border: 0;
+    color: ${theme('colors.black')};
+    outline: 0;
+
+    ${breakpoint('m')`
+      padding: ${theme('spaces.l')};
+      margin-bottom: ${theme('spaces.l')};
+      margin-top: ${theme('spaces.l')};
+      max-width: 32rem;
+    `}
+  `)};
 `
 
 const StyledLink = styled(RouterLink)`${styles}`
@@ -28,6 +81,8 @@ Link.propTypes = {
   kind: PropTypes.string,
   light: PropTypes.bool,
   to: PropTypes.string,
+  highlight: PropTypes.bool,
+  button: PropTypes.bool,
 }
 
 Link.defaultProps = {
