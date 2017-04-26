f4aa78b6a79147233a1c2db25b6afc526b211c66
Julien Vallini <vallinij@gmail.com>
2017-04-26 14:51:21 +0200

    hotfix: Reenable saga on ssr (#333)

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,11 @@
 import generateHtml from './generateHtml'
-// import initSagas from './initSagas'
+import initSagas from './initSagas'
 import initStore from './initStore'
 
 export default async function (store, renderProps, req, res) {
   initStore(store, req)
 
-  // @TODO: re-enable it once the new certificates are live
-  // await initSagas(store, renderProps)
+  await initSagas(store, renderProps)
 
   const rendered = generateHtml(store, renderProps)
 
