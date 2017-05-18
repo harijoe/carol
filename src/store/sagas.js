a65a4ba4620cccafaba8bcaaf73db6943317909a
Julien Vallini <vallinij@gmail.com>
2017-05-18 17:27:41 +0200

    hotfix: fix bug with default export

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -11,7 +11,18 @@ req.keys().forEach((key) => {
   const elements = req(key)
 
   forIn(elements, (element, name) => {
-    module.exports[name] = element
+    /*
+     Explanation : Without it `sagas` doesn't contain all of the sagas, but only the last domain's sagas
+       (which is user at this time). This occurs only when NODE_ENV is set to production. The solution proposed here is
+       to filter the sagas imported in store/sagas to avoid setting module.exports.default. Thus the ES5 default export
+       feature is not conflicting with modules.export.default
+
+     Notice : If you find the reason behind this behavior, please send a mail explaining what you
+      understood to vallinij@gmail.com
+     */
+    if (name === 'default') {
+      module.exports[name] = element
+    }
   })
 })
 
