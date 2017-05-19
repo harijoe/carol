1882c371c07b3cb370d3ea4515126dcb7a8940b1
Gueorgui Agapov <7s4r@users.noreply.github.com>
2017-05-19 16:38:39 +0200

    hotfix: redirect (#429)
    * fix
    
    * :)
    
    * fix

--- src/store/user/sagas.js
+++ src/store/user/sagas.js
@@ -1,8 +1,8 @@
 import { put, select } from 'redux-saga/effects'
 import { stopSubmit, reset } from 'redux-form'
-import { push } from 'react-router-redux'
-import { api } from 'config'
+import { push, goBack } from 'react-router-redux'
 import { fromUser, fromRouting } from 'store/selectors'
+import { handleAuthLoginRequest } from 'store/sagas'
 import { HTTPError } from 'utils/errors'
 import fetch from 'sagas/fetch'
 import notify from 'sagas/notify'
@@ -30,19 +30,17 @@ import {
     verifyEmail,
     resendEmail,
 } from './actions'
-import { authLogin } from '../auth/actions'
 
 function* handleCreateUserRequest({ data }) {
   try {
     yield* fetch(userCreate, 'post', '/users', {}, data)
     yield* notify('user.thank_you', 'user.sign_up.confirmation')
-    yield* fetch(
-      authLogin('password', 'SignUpForm'),
-      'get',
-      `/oauth/v2/token?client_id=${api.clientId}&client_secret=${api.clientSecret}&grant_type=password&username=${data.email}&password=${data.password}`,
-      {},
-    )
-    yield put(push('/'))
+    yield* handleAuthLoginRequest({
+      grantType: 'password',
+      formName: 'SignUpForm',
+      credentials: `&username=${data.email}&password=${data.password}`,
+    })
+    yield put(goBack())
   } catch (error) {
     if (error instanceof HTTPError) {
       yield put(stopSubmit('SignUpForm', getFormErrors(error.message)))
