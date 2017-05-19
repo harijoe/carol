9d1dca109c0a6aad50b23833002cf65adde79ad1
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-05-19 14:50:32 +0200

    Reduce duplication of code between takeEvery and takeLatest

--- src/utils/effects/index.js
+++ src/utils/effects/index.js
@@ -1,28 +1,6 @@
 import { fork, cancel, take } from 'redux-saga/effects'
 
-export const takeEvery = function* takeEvery(pattern, saga, ...args) {
-  const task = yield fork(function* () {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      const action = yield take(pattern)
-
-      const safeSaga = function* () {
-        try {
-          yield* saga(action)
-        } catch (e) {
-          // @TODO: To remove before production
-          console.error(e.toString())
-        }
-      }
-
-      yield fork(safeSaga, ...args.concat(action))
-    }
-  })
-
-  return task
-}
-
-export const takeLatest = function* takeLatest(pattern, saga, ...args) {
+const takeFlow = ({ cancelLastTask = false } = {}) => function* takeEvery(pattern, saga, ...args) {
   const task = yield fork(function* () {
     let lastTask
 
@@ -39,7 +17,7 @@ export const takeLatest = function* takeLatest(pattern, saga, ...args) {
         }
       }
 
-      if (lastTask) {
+      if (cancelLastTask && lastTask) {
         yield cancel(lastTask) // cancel is no-op if the task has already terminated
       }
 
@@ -49,3 +27,7 @@ export const takeLatest = function* takeLatest(pattern, saga, ...args) {
 
   return task
 }
+
+export const takeEvery = takeFlow()
+
+export const takeLatest = takeFlow({ cancelLastTask: true })
