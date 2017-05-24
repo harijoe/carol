472ea118d37af6a09ea548e9d304a39af3405f3b
Julien Vallini <vallinij@gmail.com>
2017-05-24 10:53:45 +0200

    Hotfix: Remove refresh token on logout (#455)

--- src/sagas/removeToken/index.js
+++ src/sagas/removeToken/index.js
@@ -3,6 +3,7 @@ import cookie from 'react-cookie'
 export default function* removeToken() {
   yield [
     cookie.remove('access_token'),
+    cookie.remove('refresh_token'),
     cookie.remove('grant_type'),
   ]
 }
