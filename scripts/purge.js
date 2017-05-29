f7b3df0d9c45b7d9546688e59b7d1846db9463ba
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-05-29 14:15:07 +0200

    hotfix: add SSR purge script for Sylvain and Julien (#494)

---
+++ scripts/purge.js
@@ -0,0 +1,13 @@
+require('babel-polyfill')
+require('babel-core/register')
+
+const fetch = require('isomorphic-fetch')
+const purgeCacheToken = require('../src/config')
+
+fetch('https://carol-fr-dev.qarx.io', {
+  method: 'PURGE',
+  headers: { authorization: `Bearer ${purgeCacheToken}` },
+})
+  .then(() => console.info('SSR cache purged'))
+  .catch(e => console.error('SSR cache purge failed:', e.message, e.stack.replace(/^.*/, '')))
+
