3c1d46e835e3b65ba54d6fcb53db82050bc4bbe2
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-05-30 14:23:09 +0200

    hotfix: robots no follow on project elaboration page (#506)
    For SEO purposes add a robots no-follow meta to the project elaboration page (HIANDU-944)

--- src/components/pages/ProjectElaborationPage/index.js
+++ src/components/pages/ProjectElaborationPage/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
 import { ProjectElaborationLayout } from 'components'
 import { ProjectElaboration } from 'containers'
 
 const ProjectElaborationPage = ({ location, ...props }) => (
   <ProjectElaborationLayout {...props}>
+    <Helmet>
+      <meta name="robots" content="noindex, nofollow" />
+    </Helmet>
     <ProjectElaboration {...{ location }} />
   </ProjectElaborationLayout>
 )
