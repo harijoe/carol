05bdaf653b74464c991db90eb76fde8fe43051d9
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-05-30 16:20:57 +0200

    hotfix: For SEO purposes, add json ld to the home page (#507)

--- src/components/Html.js
+++ src/components/Html.js
@@ -12,6 +12,7 @@ const Html = ({ styles, assets, state, content, lang }) => {
       <head>
         {helmet.title.toComponent()}
         {helmet.meta.toComponent()}
+        {helmet.script.toComponent()}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <style dangerouslySetInnerHTML={{ __html: styles }} />
