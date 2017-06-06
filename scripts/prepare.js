3cf4f271d7ad3f083d30299bdbd943bbe96ef346
Julien Vallini <vallinij@gmail.com>
2017-06-06 14:54:46 +0200

    Remove webpack assets from project (#544)

---
+++ scripts/prepare.js
@@ -0,0 +1,12 @@
+const execSync = require('child_process').execSync
+const fs = require('fs')
+const env = (typeof process.env.NODE_ENV !== 'undefined' && process.env.NODE_ENV.toString()) || 'development'
+
+if (env === 'development') {
+  if (!fs.existsSync('webpack/webpack-assets.json')) {
+    console.info('No webpack assets found in development environment, rebuilding it')
+    console.info('...')
+    execSync('yarn run build:development')
+    console.info('done!')
+  }
+}
