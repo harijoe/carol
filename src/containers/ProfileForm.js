450f7654fa8cd8b12d5902a7424039c5e39398f7
Thibaut MAREILLE <Tibze@users.noreply.github.com>
2017-06-12 10:21:06 +0200

    Feature/profile remove birthday (#569)

--- src/containers/ProfileForm.js
+++ src/containers/ProfileForm.js
@@ -37,7 +37,7 @@ const mapStateToProps = (state) => {
     },
     initialValues: {
       ...pick(details, [
-        '@id', 'civility', 'firstName', 'lastName', 'birthday',
+        '@id', 'civility', 'firstName', 'lastName',
         'email', 'preferedLanguage', 'mobilePhone', 'mobilePhone',
         'fixedPhone', 'address', 'postalCode', 'countryCode',
         'region', 'city', 'newsletterSubscription', 'contactPreference',
