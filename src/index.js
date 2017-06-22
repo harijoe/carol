50bd686c6ef83db6e9b6bc6ce7d1be9cbd407732
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-06-22 11:03:17 +0200

    hotfix: log unhandled rejections (#613)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 require('babel-polyfill')
 require('babel-core/register')
 
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at:', p, 'reason:', reason)
+})
+
 const WebpackIsomorphicTools = require('webpack-isomorphic-tools')
 const webpackIsomorphicToolsConfig = require('../webpack/webpack-isomorphic-tools')
 const path = require('path')
