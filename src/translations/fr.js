75b6cec65d778b37700f9ed1a2ea1ad5a8a7919d
Julien Vallini <vallinij@gmail.com>
2017-06-22 15:43:42 +0200

    Hotfix: Update title fr (#617)

--- src/translations/fr.js
+++ src/translations/fr.js
@@ -307,7 +307,7 @@ const fr = {
   'auto-validation.title.project': 'Votre projet',
 
   'pages.default.titleTemplate': 'Devis Travaux - %s',
-  'pages.default.title': 'Comparer les prix avec 5 devis gratuits',
+  'pages.default.title': 'Trouver le bon artisan avec Quotatis : comparez les devis et réalisez vos travaux',
   'pages.default.meta.description': 'Quotatis vous permet d&apos;obtenir 5 devis gratuits sous trois jours. Demandes de devis comparatifs au 01.77.86.80.97 : devis travaux, devis pompe a chaleur, devis chauffage, devis defiscalisation, devis fenêtres, devis assurances, devis piscine, devis fenetres, devis climatisation, devis panneaux solaire photovoltaiques....',
   'pages.default.meta.og.title': 'Devis Travaux - Comparer les prix avec 5 devis gratuits',
 
