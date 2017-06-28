f44bf1c52dc5086e9b28559cb4f4425d9a5b28a7
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-06-28 15:02:19 +0200

    hotfix: remove the pros card (page will also be removed as part of next sprint) (#644)

--- src/components/organisms/FirmList/index.js
+++ src/components/organisms/FirmList/index.js
@@ -2,76 +2,10 @@ import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { theme, breakpoint, breakpointMax } from 'utils/style'
+import { theme, breakpoint } from 'utils/style'
 
-import { Firm, Loading, Link, Row, Card, Carousel, Heading, Paragraph, Icon, Grid } from 'components'
+import { Loading, Link, Heading, Paragraph, Icon, Grid } from 'components'
 import { FirmAcceptButton } from 'containers'
-
-const StyledCard = styled(Card)`
-  position: relative;
-  height: 100%;
-  width: 100%;
-
-  ${breakpointMax('m')`
-    width: calc(100vw - ${theme('spaces.xl')} - ${theme('spaces.m')});
-    margin-left: ${theme('spaces.s')};
-    margin-right: ${theme('spaces.s')};
-  `}
-`
-
-const StyledRow = styled(Row)`
-  ${breakpoint('m')`
-    margin-left: calc(${theme('spaces.l')} / -2);
-    margin-right: calc(${theme('spaces.l')} / -2);
-    order: 3;
-  `}
-
-  .slick-list {
-    padding-bottom: ${theme('spaces.xl')};
-    padding-top: ${theme('spaces.xl')};
-    width: 100vw;
-  }
-
-  .slick-dots {
-    max-width: 32rem;
-  }
-
-  ${breakpoint('m')`
-    > div {
-      width: 100%;
-      margin-top: ${theme('spaces.l')};
-      margin-left: ${theme('spaces.l')};
-      margin-right: ${theme('spaces.l')};
-
-      > div {
-        display: flex;
-        flex-wrap: wrap;
-
-        > div {
-          max-width: 50%;
-          width: 100%;
-          padding: calc(${theme('spaces.l')} / 2);
-        }
-      }
-    }
-  `}
-
-  ${breakpoint('l')`
-    > div > div > div {
-      max-width: 33.33%;
-    }
-  `}
-`
-
-const NumberOfList = styled(Paragraph)`
-  position: absolute;
-  right: 0;
-  top: -${theme('spaces.m')};
-
-  ${breakpoint('m')`
-    display: none;
-  `}
-`
 
 const StyledLink = styled(Link)`
   position: absolute;
@@ -111,35 +45,6 @@ const FirmList = ({ list, loading, projectId }) => (
       </StyledParagraph>
     </Grid>
     <FirmAcceptButton projectId={projectId} />
-    <StyledRow>
-      <Carousel
-        slidesToShow={1}
-        slidesToScroll={1}
-        variableWidth
-        infinite={false}
-        dots
-        swipe
-        responsive={[
-          {
-            breakpoint: 575,
-            settings: {
-              arrows: false,
-            },
-          },
-          {
-            breakpoint: 10000,
-            settings: 'unslick',
-          },
-        ]}
-      >
-        {list.map((items, i) =>
-          <StyledCard key={i}>
-            <NumberOfList><b>{i + 1}</b>/{list.length}</NumberOfList>
-            <Firm {...items} />
-          </StyledCard>
-        )}
-      </Carousel>
-    </StyledRow>
   </Loading>
 )
 
