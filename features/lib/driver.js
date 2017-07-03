840869571139a7edde262d7ec2a83568225c3aaa
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-07-03 15:36:52 +0200

    hotfix: disable chrome headless as it is too unstable (#657)

--- features/lib/driver.js
+++ features/lib/driver.js
@@ -1,15 +1,7 @@
 import 'chromedriver'
 import { Builder } from 'selenium-webdriver'
-import chrome from 'selenium-webdriver/chrome'
 
 const builder = new Builder().forBrowser('chrome')
-
-if (process.env.CHROME_HEADLESS) {
-  const chromeOptions = new chrome.Options()
-
-  chromeOptions.addArguments('headless', 'disable-gpu')
-  builder.setChromeOptions(chromeOptions)
-}
 
 const driver = builder.build()
 
