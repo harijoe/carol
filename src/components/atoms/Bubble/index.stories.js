871544549daf7a9be403d464ad5cacd8f1475a8b
sylvain-dion <sylvain.dion@gmail.com>
2017-07-06 11:31:38 +0200

    hotfix: delete Atom Bubble (#679)

--- src/components/atoms/Bubble/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { Bubble } from 'components'
-
-storiesOf('Bubble', module)
-  .add('Default', () => (
-    <Bubble>Sample Text</Bubble>
-  ))
