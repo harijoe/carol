2ca0a2e7334b1a950b7e5c94f40a14de7ff88a9d
Julien Vallini <vallinij@gmail.com>
2017-07-07 16:25:34 +0200

    Hotfix: Fix ssr cache storage on frontside (#703)

--- src/sagas/ssr/cache.js
+++ src/sagas/ssr/cache.js
@@ -1,7 +1,11 @@
+import { locales } from 'config'
+
 /*
  This file is used to store cached response during ssr
  */
 const storage = {}
+
+Object.keys(locales).map(locale => (storage[locale] = {}))
 
 export const reset = locale => (storage[locale] = {})
 
