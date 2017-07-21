61108227e31c318161c7b6ab9530ae6e124256c0
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-07-21 15:44:39 +0200

    hotfix: remove space from json patch (#779)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -xe
+
 echo "/!\ BRANCH == ${BRANCH} /!\ "
 
 if [ $BRANCH == "develop" ]; then
@@ -16,19 +18,23 @@ if [ $BRANCH == "develop" ]; then
 
 
   gcloud docker -- push eu.gcr.io/quotatis-152617/carol:${BRANCH}
-  kubectl create ns ${BRANCH}
+  kubectl create ns ${BRANCH} || true
   kubectl get svc --namespace=develop dora-develop -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
   while [ $? -ne 0 ]; do
       kubectl get svc --namespace=develop dora-develop -o jsonpath='{.status.loadBalancer.ingress[0].ip}';
   done;
-  kubectl run carol-${BRANCH} --namespace=${BRANCH} --image=eu.gcr.io/quotatis-152617/carol:${BRANCH} --port=443  --replicas=1 --labels="branch=${BRANCH},app=quotatis,run=carol-${BRANCH}" --env="GIT_SHA1=${GIT_SHA1}" --env="PUB_IP=${CAROL_IP}" --env="NODE_ENV=qa" --env="PORT=443" --env="API_PORT=443" --env="API_IP=`kubectl get svc --namespace=develop dora-develop -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`" --image-pull-policy=Always
-  kubectl expose --port=443 deploy carol-${BRANCH} --type=LoadBalancer --name=carol-${BRANCH} --namespace=${BRANCH}
+  kubectl run carol-${BRANCH} --namespace=${BRANCH} --image=eu.gcr.io/quotatis-152617/carol:${BRANCH} --port=443  --replicas=1 \
+            --labels="branch=${BRANCH},app=quotatis,run=carol-${BRANCH}" --env="GIT_SHA1=${GIT_SHA1}" --env="PUB_IP=${CAROL_IP}" \
+            --env="NODE_ENV=qa" --env="PORT=443" --env="API_PORT=443" \
+            --env="API_IP=`kubectl get svc --namespace=develop dora-develop -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`" \
+            --image-pull-policy=Always  || true
+  kubectl expose --port=443 deploy carol-${BRANCH} --type=LoadBalancer --name=carol-${BRANCH} --namespace=${BRANCH} || true
   kubectl get svc --namespace=${BRANCH} carol-${BRANCH} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
   while [ $? -ne 0 ]; do
      kubectl get svc --namespace=${BRANCH} carol-${BRANCH} -o jsonpath='{.status.loadBalancer.ingress[0].ip}';
   done;
   export CAROL_IP=`kubectl get svc --namespace=${BRANCH} carol-${BRANCH} -o jsonpath="{.status.loadBalancer.ingress[0].ip}"`
-  export JSON_PATCH=`echo '{"spec":{"template":{"spec":{"containers":[{"env":[{"name":"API_IP","value":"104.199.107.196"}, {"name":"GIT_SHA1","value":"GIT_SHA1_VALUE"}],"name":"carol-BRANCH"}]}}}}' | sed s/BRANCH/$BRANCH/g | sed s/GIT_SHA1_VALUE/$GIT_SHA1/g`
+  export JSON_PATCH=`echo '{"spec":{"template":{"spec":{"containers":[{"env":[{"name":"API_IP","value":"104.199.107.196"},{"name":"GIT_SHA1","value":"GIT_SHA1_VALUE"}],"name":"carol-BRANCH"}]}}}}' | sed s/BRANCH/$BRANCH/g | sed s/GIT_SHA1_VALUE/$GIT_SHA1/g`
   echo ${JSON_PATCH}
   kubectl patch deployment carol-${BRANCH} --namespace=${BRANCH} -p $JSON_PATCH
   kubectl delete pods `kubectl get pod -l "run=carol-${BRANCH}" -o=template --template="{{ with index .items 0}}{{ .metadata.name }}{{ end }}" --namespace=${BRANCH}` --namespace=${BRANCH}
