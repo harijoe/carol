0989f8d01407cb08b02ee135d7d9120c93628b1e
Thibaut MAREILLE <Tibze@users.noreply.github.com>
2017-07-31 09:49:00 +0200

    Feature: create empty result page (#804)

---
+++ src/components/pages/SearchResultPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { MainLayout, Paragraph } from 'components'
+
+const SearchResultPage = () => (
+  <MainLayout>
+    <Paragraph>Blank Search Result Page</Paragraph>
+  </MainLayout>
+)
+
+export default SearchResultPage
