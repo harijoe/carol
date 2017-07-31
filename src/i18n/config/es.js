923935e23f833dec9529329bf7c9b33ec471c9dc
Thibaut MAREILLE <Tibze@users.noreply.github.com>
2017-07-31 15:17:57 +0200

    Feature: es home configuration (#808)

--- src/i18n/config/es.js
+++ src/i18n/config/es.js
@@ -1,13 +1,14 @@
 export default {
   url: 'https://carol-es-dev.qarx.io',
   countryLabel: 'country.spain',
-  contentSiteUrl: 'https://www.quotatis.es/advice/',
+  contentSiteUrl: 'https://www.quotatis.es/consejos-reformas/',
   mainMenu: {
     resource: {
-      guide: { path: 'https://noticias.quotatis.es/', target: '_blank' },
+      guide: { path: 'https://www.quotatis.es/consejos-reformas/guia/', target: '_blank' },
+      faq: { path: 'https://www.quotatis.es/consejos-reformas/preguntas-frecuentes/', target: '_blank' },
     },
-    directory: '/tradesmen-directory',
-    proUrl: 'https://www.quotatispro.co.uk',
+    directory: '/directorio-empresas',
+    proUrl: 'https://www.quotatispro.es',
   },
   homepage: {
     verifiedProsLink: '/tradesmen-directory',
@@ -16,50 +17,53 @@ export default {
     advices: {
       column_1_1: {
         key: 'solar_pv',
-        src: 'guide/solar-panels/solar-pv/',
+        src: 'guia/reformas/reforma-de-bano/',
       },
       column_1_2: {
         key: 'windows',
-        src: 'guide/windows-doors/windows/',
+        src: 'guia/reformas/cocina/',
+      },
+      column_1_3: {
+        key: 'conservatories',
+        src: 'guia/pintura-suelos-y-paredes/pintura/',
       },
       column_2_1: {
-        key: 'conservatories',
-        src: 'guide/conservatories-roofing-extensions/conservatories/',
+        key: 'gas_boiler',
+        src: 'guia/albanileria-carpinteria-y-puertas/albanileria/',
       },
       column_2_2: {
-        key: 'gas_boiler',
-        src: 'guide/heating-air-con/gas-boilers/',
+        key: 'driveways',
+        src: 'guia/albanileria-carpinteria-y-puertas/carpinteria/',
       },
       column_3_1: {
-        key: 'driveways',
-        src: 'guide/paving-driveways/driveways/',
+        key: 'pitched_roofing',
+        src: 'guia/pintura-suelos-y-paredes/suelos/',
       },
       column_3_2: {
-        key: 'patios',
-        src: 'guide/paving-driveways/patios/',
+        key: 'flat_roofing',
+        src: 'guia/aislamiento-calefaccion-aire-acondicionado/aire-acondicionado/',
       },
       column_4_1: {
-        key: 'pitched_roofing',
-        src: '?s=pitched+roofing',
+        key: 'domestic_hot_water_production',
+        src: 'guia/ventanas-y-aperturas-exteriores/ventanas/',
       },
       column_4_2: {
-        key: 'flat_roofing',
-        src: 'guide/conservatories-roofing-extensions/flat-roofing/',
+        key: 'patios',
+        src: 'guia/albanileria-carpinteria-y-puertas/puertas/',
       },
     },
     corporate: {
-      contact: '/contact.html',
-      about: 'who-we-are',
-      use_conditions: 'privacy-policy',
+      contact: 'contactanos.html',
+      about: 'quien-somos',
+      use_conditions: 'menciones-legales',
       guides: '',
-      directory: '/tradesmen-directory',
       qpro: 'https://www.quotatispro.es/',
     },
     social: {
-      facebook: 'https://www.facebook.com/Quotatis',
-      twitter: 'https://twitter.com/Quotatis',
+      facebook: 'https://www.facebook.com/Quotatis.Es',
+      twitter: 'https://twitter.com/QuotatisEspana',
       google: 'https://plus.google.com/106376832204465138273',
-      youtube: 'https://www.youtube.com/user/quotatisuk',
+      youtube: 'https://www.youtube.com/user/quotatises',
       pinterest: 'https://www.pinterest.com/quotatis/',
     },
   },
