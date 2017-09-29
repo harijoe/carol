be60e0fa92977da6be21b8fd7c9bc90d0dd8fb49
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-09-29 16:36:43 +0200

    hotfix: better cucumber debug  (#1061)

--- features/support/debug.js
+++ features/support/debug.js
@@ -1,10 +1,18 @@
 import { defineSupportCode, Status } from 'cucumber'
 
+const keyPressed = () => new Promise(resolve => {
+  const stdin = process.openStdin()
+  stdin.setRawMode(true)
+  stdin.resume()
+  stdin.on('data', resolve)
+})
+
+
 defineSupportCode(({ After }) => {
   After({ timeout: 1000 * 1000 * 1000 }, async ({ result: { status } }) => {
     if (status === Status.FAILED && process.env.DEBUG) {
-      console.info('\nFailure detected while in DEBUG mode. Press Ctrl+C to interrupt.')
-      await new Promise(() => {})
+      console.info('\nFailure detected while in DEBUG mode - Hit any key')
+      await keyPressed()
     }
   })
 })
