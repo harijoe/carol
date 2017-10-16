9a76f8c32c0a0e15f61a0ffd139d02d2678625d8
Benoit d'Oncieu <bdoncieu@gmail.com>
2017-10-16 15:49:23 +0200

    hotfix: ios 11 popin scroll issue (#1100)

--- src/components/atoms/Popin/index.js
+++ src/components/atoms/Popin/index.js
@@ -6,32 +6,31 @@ import { theme, ifThen, breakpoint, mapBreakpoints } from 'utils/style'
 
 const OuterWrapper = styled.div`
   ${({ show }) => css`
-  ${ifThen(
-    show,
-    `
+    ${ifThen(
+      show,
+      `
     height: 100vh;
     opacity: 1;
     z-index: 50;
   `,
-    `
+      `
     height: 0;
     opacity: 0;
     z-index: -50;
   `,
-  )}
-  position: fixed;
-  left: 0;
-  top: 0;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-  width: 100%;
-  background: white;
-  transition: height 0.3s linear, padding-top 0.3s linear, padding-bottom 0.3s linear, border-top-width 0.3s linear, border-top-width 0.3s linear${ifThen(
-    !show,
-    ', opacity 0.1s linear 1s',
-  )};
-  ${breakpoint('m')`
+    )} position: fixed;
+    left: 0;
+    top: 0;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    width: 100%;
+    background: white;
+    transition: height 0.3s linear, padding-top 0.3s linear, padding-bottom 0.3s linear, border-top-width 0.3s linear, border-top-width 0.3s linear${ifThen(
+      !show,
+      ', opacity 0.1s linear 1s',
+    )};
+    ${breakpoint('m')`
     height: 100vh;
     align-items: center;
     justify-content: center;
@@ -39,24 +38,23 @@ const OuterWrapper = styled.div`
     background: transparent;
     transition: opacity 0.2s linear;
     ${ifThen(!show, 'pointer-events: none;', '')}
-  `}
-`};
+  `};
+  `};
 `
 
 const InnerWrapper = styled.div`
   ${({ show }) => css`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-color: ${theme('colors.white')};
-  ${ifThen(show, '', 'display: none')};
-  pointer-events: auto;
-  ${mapBreakpoints(
-    bp => css`
-    padding: ${theme(`grid.gutterWidth.${bp}`, 'rem')};
-  `,
-  )}
-  ${breakpoint('m')`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background-color: ${theme('colors.white')};
+    ${ifThen(show, '', 'display: none')};
+    pointer-events: auto;
+    ${mapBreakpoints(
+      bp => css`
+        padding: ${theme(`grid.gutterWidth.${bp}`, 'rem')};
+      `,
+    )} ${breakpoint('m')`
     ${ifThen(show, 'top: 0;', 'top: -50px;')}
     margin: 5rem auto;
     max-width: 50rem;
@@ -64,8 +62,8 @@ const InnerWrapper = styled.div`
     opacity: 1;
     transition: top 0.2s linear;
     height: auto;
-  `}
-`};
+  `};
+  `};
 `
 
 const ScrollWrapper = styled.div`
@@ -85,16 +83,33 @@ const Background = styled.div`
   `};
 `
 
-const Popin = ({ children, show, closeAll }) =>
-  <OuterWrapper show={show}>
-    <Background onClick={closeAll} />
-    <ScrollWrapper>
-      <InnerWrapper className={`popin ${show ? 'visible' : ''}`} show={show}>
-        <CloseAllButton />
-        {show && children}
-      </InnerWrapper>
-    </ScrollWrapper>
-  </OuterWrapper>
+const onTransitionEnd = (e, innerWrapper) => {
+  if (e.propertyName === 'height' && innerWrapper !== null) {
+    // eslint-disable-next-line no-param-reassign
+    innerWrapper.style.height = 'auto'
+  }
+}
+
+const Popin = ({ children, show, closeAll }) => {
+  let innerWrapper = null
+  return (
+    <OuterWrapper show={show} onTransitionEnd={e => onTransitionEnd(e, innerWrapper)}>
+      <Background onClick={closeAll} />
+      <ScrollWrapper>
+        <InnerWrapper
+          className={`popin ${show ? 'visible' : ''}`}
+          show={show}
+          innerRef={ref => {
+            innerWrapper = ref
+          }}
+        >
+          <CloseAllButton />
+          {show && children}
+        </InnerWrapper>
+      </ScrollWrapper>
+    </OuterWrapper>
+  )
+}
 
 Popin.propTypes = {
   closeAll: PropTypes.func,
